9e9abe5b8f5d5d23fc6ddcd945fa58a6c56fb31f
Christian Krause <me@ckrause.org>
2023-03-05 15:25:31 +0100

    add package docstring

---
+++ loda/__init__.py
@@ -0,0 +1,5 @@
+"""Python implementation of the [LODA Language](https://loda-lang.org/). 
+
+You can use it to read, write LODA programs, evaluate them to integer sequences
+and use machine learning and mining methods to find new programs for integer sequences.
+"""
