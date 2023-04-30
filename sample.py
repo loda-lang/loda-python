fcd7785ee9a59e11fe3892bd327cb74a9e0076c1
Christian Krause <me@ckrause.org>
2023-04-30 22:55:08 +0200

    simplify sample program

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 import os.path
 
+from loda.lang import Program
 from loda.oeis import ProgramCache, Sequence
 from loda.runtime import Evaluator, Interpreter
 from loda.mine import Miner
@@ -9,53 +10,29 @@ from loda.ml.keras.program_generation_rnn import load_model, train_model, Genera
 class SampleLODA:
 
     def __init__(self):
-        # Initialize LODA programs cache using *.asm files from tests folder
+        # Initialize LODA programs cache using *.asm files from tests folder.
         programs_dir = os.path.join("tests", "programs", "oeis")
         # programs_dir = os.path.expanduser("~/loda/programs/oeis")
         self.program_cache = ProgramCache(programs_dir)
         self.interpreter = Interpreter(self.program_cache)
 
     def print_program(self):
-        # Load the LODA program for the prime numbers (A000040.asm)
-        # See also the integer sequence entry at https://oeis.org/A000040
-        program = self.program_cache.get(40)  # numeric version of A000040
+        # Load the LODA program for the Fibonacci numbers (A000045.asm).
+        # See also the integer sequence entry at https://oeis.org/A000045.
+        program = self.program_cache.get(45)  # numeric version of A000045
         print(program)
 
     def eval_program_to_seq(self):
-        # Evaluate the program to an integer sequence
-        program = self.program_cache.get(40)  # numeric version of A000040
-        evaluator = Evaluator(program, self.interpreter)
-        for _ in range(10):
-            print(evaluator())
-
-    def mine(self):
-        model = train_model(self.program_cache, num_programs=1000)
-        model.save("sample_model")
-
-        # Load the model back from disk.
-        loaded = load_model("sample_model")
-        loaded.summary()
-
-        # Use the trained model to generate programs.
-        generator = Generator(loaded, num_lanes=10)
-
-        for _ in range(10):
-            print(generator())
-
-        existing_ids = set(self.program_cache.all_ids())
-        seqs = Sequence.load_oeis(os.path.expanduser("~/loda/oeis"))
-        seqs = list(filter(lambda s:
-                           len(s.terms) >= 8 and s.id not in existing_ids, seqs))
-        print("Loaded {} sequences".format(len(seqs)))
-        miner = Miner(seqs, self.interpreter, generator)
-        for i in range(20):
-            miner()
-            if i % 10 == 0:
-                print(generator.get_stats_info_str())
+        # Evaluate the program to an integer sequence.
+        # This time we load it from a file.
+        with open("fibonacci.asm", "r") as file:
+            program = Program(file.read())
+            evaluator = Evaluator(program, self.interpreter)
+            for _ in range(10):
+                print(evaluator())
 
 
 if __name__ == "__main__":
     sample = SampleLODA()
     sample.print_program()
     sample.eval_program_to_seq()
-    sample.mine()
